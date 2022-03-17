a1cbd38b7f5b19422614ffacff43c74b274c8342
Ismail Palali <ismail.palali.pp@gmail.com>
2022-03-17 23:13:17 +0300

    Finish app

--- TodoAppSimple/ViewController.swift
+++ TodoAppSimple/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         getData()
     }
-
+    
     func getData() {
         let tasks = DatabaseHelper.shareInstance.fetch()
         
@@ -24,6 +24,27 @@ class ViewController: UIViewController {
         tableView.reloadData()
     }
     @IBAction func btnAdd(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add Task", message: nil, preferredStyle: .alert)
+        
+        let add = UIAlertAction(title: "Add", style: .default) { _ in
+            
+            // save
+            guard let name = alert.textFields?.first?.text else  {return}
+            print(name)
+            DatabaseHelper.shareInstance.save(name: name, isDone: false)
+            self.getData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Enter task name..."
+        }
+        
+        alert.addAction(add)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
     
 }
@@ -46,6 +67,38 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return taskStore.count
     }
+}
+
+extension ViewController {
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let delete = UIContextualAction(style: .normal, title: nil) {(action,sourceView,completionHander)in
+        
+            let row = self.taskStore[indexPath.section][indexPath.row]
+            DatabaseHelper.shareInstance.delete(name: row.name!)
+            self.getData()
+        }
+        
+        delete.image = UIImage(named: "trash-24.png")
+        delete.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let done = UIContextualAction(style: .normal, title: nil) {(action,sourceView,completionHander)in
+        
+            let row = self.taskStore[0][indexPath.row]
+            DatabaseHelper.shareInstance.update(name: row.name!, isDone: true)
+            self.getData()
+        }
+        
+        done.image = UIImage(named: "checkmark-24.png")
+        done.backgroundColor = #colorLiteral(red: 0.1843137255, green: 0.7529411765, blue: 0.6588235294, alpha: 1)
+        
+        return indexPath.section == 0 ? UISwipeActionsConfiguration(actions: [done]) : nil
+    }
 }
 
