6c4d5b51aa0f05e0e3f39c286b23ba90f3e920a8
Ismail Palali <ismail.palali.pp@gmail.com>
2022-03-17 22:04:07 +0300

    Add tableview

--- TodoAppSimple/ViewController.swift
+++ TodoAppSimple/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var taskStore = [[TaskEntity](),[TaskEntity]()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
     }
 
+    func getData() {
+        let tasks = DatabaseHelper.shareInstance.fetch()
+        
+        taskStore = [tasks.filter{$0.isdone == false},tasks.filter{$0.isdone == true}]
+        
+        tableView.reloadData()
+    }
+    @IBAction func btnAdd(_ sender: UIBarButtonItem) {
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskStore[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = taskStore[indexPath.section] [indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "To-do" : "Done"
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return taskStore.count
+    }
+    
 }
 
