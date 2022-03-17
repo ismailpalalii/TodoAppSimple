6ec552536ea8c2a4a18e096c3e2471d70dbfa9ab
Ismail Palali <ismail.palali.pp@gmail.com>
2022-03-17 21:02:51 +0300

    Initial Commit

---
+++ TodoAppSimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoAppSimple
+//
+//  Created by ismail palali on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
